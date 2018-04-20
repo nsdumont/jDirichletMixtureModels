8fe630fa389fc22cf11f5add227505bb917f782c
Kira Aveline Selby <kaselby@uwaterloo.ca>
2018-04-20 13:10:46 -0400

    Fixed stuff.

--- R/dmm_functions.R
+++ R/dmm_functions.R
@@ -24,8 +24,8 @@ dmm.setup <- function(...) {
   } else {
     .dmm$julia <- JuliaCall::julia_setup("/Applications/Julia-0.6.app/Contents/Resources/julia/bin/")
   }
-  .dmm$julia$install_package_if_needed("DirichletMixtureModels") 
-  .dmm$julia$library("DirichletMixtureModels") 
+  .dmm$julia$install_package_if_needed("DirichletMixtureModels")
+  .dmm$julia$library("DirichletMixtureModels")
   .dmm$julia$command("using DirichletMixtureModels")
   .dmm$julia$command("importall DirichletMixtureModels")
 }
@@ -88,35 +88,35 @@ dmm.model <- function(...){
   UseMethod("dmm.model")
 }
 
-#' @export 
+#' @export
 dmm.model.NULL <- function(...){
   return(.BaseModel())
 }
 
 #' Create a model object to be used for dmm.cluster
-#' 
+#'
 #' If a function is passed to dmm.model it will create and return an Rmodel
-#' @export  
+#' @export
 dmm.model.function <- function(...){
   arguments <- list(...)
   return(.RModel(arguments))
 }
 
 #' Create a model object to be used for dmm.cluster
-#' 
+#'
 #' If a string is passed to dmm.model it will create and return either a BaseModel (if its one string),
 #' or a JModel for 4 strings
 #' @export
 dmm.model.character <- function(...){
   arguments <- list(...)
-  
+
   if (length(arguments) == 1){
     return(.BaseModel(arguments))
-  } 
-  else if (length(arguments) == 4){
-    .add_file(arguments[1])
-    return(.JModel(arguments[2:4]))
-  } 
+  }
+  else if (length(arguments) == 5){
+    add_file(arguments[1])
+    return(.JModel(arguments[2], arguments[3], arguments[4], arguments[5]))
+  }
   else {
     stop("Improper inputs to dmm.model. Either 1 or 4 strings must be passed.")
   }
@@ -127,7 +127,7 @@ dmm.model.character <- function(...){
 #' @param filename The name of the Julia file
 #' @export
 add_file <- function(filename){
-  .dmm$julia$command(paste("include(",filename,")"))
+  .dmm$julia$command(paste0("include(\"",filename,"\")"))
 }
 
 #' Constructing RModel object: a model based on user specifed R function
@@ -147,7 +147,7 @@ add_file <- function(filename){
 }
 
 #' Constructing BaseModel object: a conjugate model given by typename
-.BaseModel <- function(typename = "MultivariateNormal", params=NULL){
+.BaseModel <- function(typename = "MultivariateNormalModel()", params=NULL){
   model <- list(model_type=typename, params = params)
   attr(model, "class") <- "BaseModel"
   return(model)
@@ -171,27 +171,27 @@ add_file <- function(filename){
 #' parameter \code{alpha}. In the 1D case, \code{Xdata} is assumed to be a 1D array of floats. In
 #' the 2D case, \code{Xdata} is assumed to be a dxN array of floats, where the data is
 #' d-dimensional and N is the number of datapoints.
-#' Returns a list of states. The elements of the list are all states 
+#' Returns a list of states. The elements of the list are all states
 #' post-burnin iteration, with the default being a \code{burnin} of 200. By default, this
 #' array is shuffled so that it may be used to approximate I.I.D draws from the
 #' posterior.
-#' 
-#' To see a formatted summary of all the clusters in a given state use the \code{dmm.summarize(clusterInfo)} function. 
-#' 
+#'
+#' To see a formatted summary of all the clusters in a given state use the \code{dmm.summarize(clusterInfo)} function.
+#'
 #'@return A list of states (i.e. \code{state = states[[i]]}). A state is itself a list.
 #' A state has two fields: \code{labeledData} and \code{clusterInfo}.
-#' 
+#'
 #' \code{labeledData} is a data.frame of the \code{Xdata} data points and their cluster labels.
 #' \code{clusterInfo} is either a list or a data.table (if the data.table package is loaded by the user). It conatins
 #' (1) cluster labels, (2) the number of data points (i.e. population) of each cluster, and (3) all of the parameters for each cluster.
-#' 
+#'
 #' If clusterInfo is a data.table, each row refers to a cluster. Columns are the cluster label, the population, and the rest of the columns are parameters.
-#' 
-#' If clusterInfo is a list, each element of the list refers to a clsuter,  clusterInfo[[i]] is a list containing of the above information for 
+#'
+#' If clusterInfo is a list, each element of the list refers to a clsuter,  clusterInfo[[i]] is a list containing of the above information for
 #' cluster i as elements. E.g. clusterInfo[[1]]$population is the population of cluster 1. The params field (clusterInfo[[i]]$params)
 #' is itself a list of each of the parameters
-#' 
-#' 
+#'
+#'
 #' @import JuliaCall
 #' @export
 dmm.cluster <- function(model, Xdata, alpha=1.0, m_prior=3, m_post=3, iters=5000, burnin=200, shuffle=TRUE){
@@ -199,7 +199,7 @@ dmm.cluster <- function(model, Xdata, alpha=1.0, m_prior=3, m_post=3, iters=5000
 }
 
 #' Use a Dirichlet Mixture Model on data to get cluster labels and cluster parameter values.
-#' 
+#'
 #' If using a user specifed model via R functions.
 #' @export
 dmm.cluster.RModel <- function(model, Xdata, alpha=1.0, m_prior=3, m_post=3, iters=5000, burnin=200, shuffle=TRUE){
@@ -208,7 +208,7 @@ dmm.cluster.RModel <- function(model, Xdata, alpha=1.0, m_prior=3, m_post=3, ite
   .dmm$julia$assign("sample_func", JuliaObject(model$sample_posterior))
   .dmm$julia$assign("marg_func", JuliaObject(model$marginal_likelihood))
   .dmm$julia$assign("params", JuliaObject(model$params))
-  
+
   if (model$isconjugate){
     .dmm$julia$command("rmodel=GeneralConjugateModel(pdf_func,sample_func,marg_func,params);")
   } else {
@@ -237,7 +237,7 @@ dmm.cluster.RModel <- function(model, Xdata, alpha=1.0, m_prior=3, m_post=3, ite
 }
 
 #' Use a Dirichlet Mixture Model on data to get cluster labels and cluster parameter values.
-#' 
+#'
 #'If using a user specifed model via Julia functions.
 #'@export
 dmm.cluster.JModel <- function(model, Xdata, alpha=1.0, m_prior=3, m_post=3, iters=5000, burnin=200, shuffle=TRUE){
@@ -274,20 +274,20 @@ dmm.cluster.JModel <- function(model, Xdata, alpha=1.0, m_prior=3, m_post=3, ite
 }
 
 #' Use a Dirichlet Mixture Model on data to get cluster labels and cluster parameter values.
-#' 
+#'
 #' If using one of the avaible bulit-in models.
-#' 
-#' 
+#'
+#'
 #' @export
 dmm.cluster.BaseModel <- function(model, Xdata, alpha=1.0, iters=5000, burnin=200, shuffle=TRUE){
   # Create julia model object given name
   # Case: model without params, ie using default parameters
   if (is.null(model$params)){
-    strcommand <- paste0("basemodel=",model$model_type,"Model();")
+    strcommand <- paste0("basemodel=",model$model_type,"();")
   # Case: model with params, ie using user defined parameters
   } else {
     .dmm$julia$assign("params", model$params)
-    strcommand <- paste0("basemodel=",model$model_type,"Model(params);")
+    strcommand <- paste0("basemodel=",model$model_type,"(params...);")
   }
   .dmm$julia$command(strcommand)
   # Converting all inputs to julia objects
@@ -298,96 +298,107 @@ dmm.cluster.BaseModel <- function(model, Xdata, alpha=1.0, iters=5000, burnin=20
   .dmm$julia$assign("burnin", burnin)
   .dmm$julia$command("burnin = Int64(burnin);")
   .dmm$julia$assign("shuffle", shuffle)
-  
+
   # Run cluster code
-  juliastates <- .dmm$julia$eval("dp_cluster(Y, basemodel, alpha, iters=iters, burnin=burnin, shuffled=shuffle);")
+  juliastates <- .dmm$julia$eval("export_r_all(Y,basemodel,
+                                 dp_cluster(Y, basemodel, alpha, iters=iters, burnin=burnin, shuffled=shuffle));")
   # Get labels/names of parameters if they exist
   paramnames <- unlist(.dmm$julia$eval("parameter_names(basemodel);"))
-  
+
   dmmstates <- dmm.states(juliastates,paramnames)
   return(dmmstates)
 }
 
 #' Constructing list of all states from dmm.cluster run (excluding burnin).
-#' 
+#'
 #' @param juliastates The object returned by Julia code
 #' @param paramnames Optionally. A list of the parameter names. Returned by Julia code for most bulit-in models.
-#' 
+#'
 #' @details Each item in the list (i.e. \code{state = states[[i]]}) is a state. A state is also a list.
 #' A state has two fields: \code{labeledData} and \code{clusterInfo}.
-#' 
+#'
 #' \code{labeledData} is a data.frame of the data points and their cluster labels.
 #' \code{clusterInfo} is either a list or a data.table (if the data.table package is loaded by the user). It conatins
 #' (1) cluster labels, (2) the number of data points (i.e. population) of each cluster, and (3) all of the parameters for each cluster.
-#' 
+#'
 #' If clusterInfo is a data.table, each row refers to a cluster. Columns are the cluster label, the population, and the rest of the columns are parameters.
-#' 
-#' If clusterInfo is a list, each element of the list refers to a clsuter,  clusterInfo[[i]] is a list containing of the above information for 
+#'
+#' If clusterInfo is a list, each element of the list refers to a clsuter,  clusterInfo[[i]] is a list containing of the above information for
 #' cluster i as elements. E.g. clusterInfo[[1]]$population is the population of cluster 1. The params field (clusterInfo[[i]]$params)
 #' is itself a list of each of the parameters
-#' 
-#' 
+#'
+#'
 #' @import JuliaCall
 dmm.states <- function(juliastates, paramnames=NULL){
   states <- list()
+  paramlen=length(juliastates[1][3])
+  if (length(paramnames) != paramlen){
+    paramnames <- sapply(1:paramlen, function(x) paste0("param.",toString(x)))
+  }
   # If user is not using data.table package
-  if (!requireNamespace("data.table", quietly = TRUE)) {
+  if (TRUE){#(!requireNamespace("data.table", quietly = TRUE)) {
     for (i in 1:length(juliastates)){
       states[[i]] <- dmm.state(juliastates[i], paramnames)
     }
   } else {
     # Otherwise use nice data.tables
     for (i in 1:length(juliastates)){
-      states[[i]] <- dmm.stateAsTable(juliastates[i], paramnames)
+      states[[i]] <- dmm.state.astable(juliastates[i], paramnames)
     }
   }
   return(states)
 }
 
-#' Formats a single DMM OutputState Julia object into a list of labeled data and list of cluster info
-#' @import JuliaCall
-dmm.state <- function(juliastate, paramnames=NULL){
-  paramlen <- length(field(juliastate,"phi")[1])
-  nums <- length(field(juliastate,"n"))
-  
-  # If any paramters names don't exist (and instead paramter_names returns NULL, which will not appear when
-  # unlisted and this paramnames won't be full size) then don't use them
-  if (length(paramnames) != paramlen){
-    paramnames <- sapply(1:nums, function(x) paste0("param.",toString(x)))
+dmm.state <- function(juliastate,paramnames){
+  state<-list()
+  data=juliastate[1]
+  state$data<- data.frame("cluster" = data[,1],"x" = data[,2:ncol(data)])
+
+  clusterinfo=list()
+  nclusters=length(juliastate[2])
+  for(i in 1:nclusters){
+    params=juliastate[2][i]
+    names(params) <- paramnames
+    attr(params[[i]], "class") <- NULL
+    clusterinfo[[i]] <- list("cluster"=i, "population"=juliastate[3][i],
+                             "params"=params)
   }
-  
-  labeledX <- data.frame("x" = field(juliastate,"data"), 
-                         "cluster" = field(juliastate,"labels"))
-  
-  parameters <- list()
-  clusterInfo <- list()
-  for (k in 1:nums){
-    parameters[[k]] <- field(juliastate,"phi")[k]
-    attr(parameters[[k]], "class") <- NULL
-    names(parameters[[k]]) <- paramnames
-    clusterInfo[[k]] <- list("cluster"=k, "population"=field(juliastate,"n")[k],
-                             "params"=parameters[[k]])
-  }
-  
-  state <- list(labeledData=labeledX, clusterInfo = clusterInfo)
-  return(state)
+  state$clusters <- clusterinfo
+  state
 }
+dmm.state.astable <- function(juliastate,paramnames){
+  state<-list()
+  data=juliastate[1]
+  state$data<- data.frame("cluster" = data[,1],"x" = data[,2:ncol(data)])
+
+  nclusters=length(juliastate[2])
+  parameters <- list()
+  for (j in 1:length(paramnames)){
+    parameters[[j]] <- juliastate[2][1:nclusters][j]
+    attr(parameters[[j]], "class") <- NULL
+  }
+  clusterinfo <- data.table("cluster" = 1:nclusters, "population" = juliastate[3])
+  clusterinfo[,(paramnames) := parameters]
+  state$clusters <- clusterinfo
+  state
+}
+
 
 #' Formats a single DMM OutputState Julia object into a list of labeled data and data.table of cluster info
 #' @import JuliaCall
 dmm.stateAsTable <- function(juliastate, paramnames=NULL){
     paramlen <- length(field(juliastate,"phi")[1])
     nums <- length(field(juliastate,"n"))
-    
+
     # If any paramters names don't exist (and instead paramter_names returns NULL, which will not appear when
     # unlisted and this paramnames won't be full size) then don't use them
     if (length(paramnames) != paramlen){
       paramnames <- sapply(1:nums, function(x) paste0("param.",toString(x)))
     }
-    
-    labeledX <- data.frame("x" = field(juliastate,"data"), 
+
+    labeledX <- data.frame("x" = field(juliastate,"data"),
                             "cluster" = field(juliastate,"labels"))
-  
+
     parameters <- list()
     for (j in 1:paramlen){
       parameters[[j]] <- field(juliastate,"phi")[1:nums][j]
@@ -395,16 +406,16 @@ dmm.stateAsTable <- function(juliastate, paramnames=NULL){
     }
     clusterInfo <- data.table("cluster" = 1:nums, "population" = field(juliastate,"n"))
     clusterInfo[,(paramnames) := parameters]
-    
+
     state <- list(labeledData=labeledX, clusterInfo = clusterInfo)
     return(state)
 }
 
 
 #' Summerize given a single state's cluster info
-#' 
+#'
 #' @param clusterInfo A list or data.table. Given states <- dmm.cluster(...), this input is states$clusterInfo
-#' 
+#'
 #' @export
 dmm.summarize <- function(clusterInfo){
   UseMethod("dmm.summarize")
@@ -425,7 +436,7 @@ dmm.summarize.data.table <- function(clusterInfo){
       }
     }
   }
-} 
+}
 
 #' @export
 dmm.summarize.list <- function(clusterInfo){
@@ -442,9 +453,9 @@ dmm.summarize.list <- function(clusterInfo){
       }
     }
   }
-} 
+}
 
-#' 
+#'
 #' @export
 dmmm.plot <- function(labeledData){
   if (ncol(labeledData) == 3){
@@ -453,8 +464,8 @@ dmmm.plot <- function(labeledData){
     if (!requireNamespace("ggplot2", quietly = TRUE)) {
       p <- ggplot( labeledData, aes(x=x.1, y=x.2 ) ) +
         geom_point(aes(colour = factor(cluster)),alpha=0.8) +
-        xlab(expression(x[1])) + 
-        ylab(expression(x[2])) 
+        xlab(expression(x[1])) +
+        ylab(expression(x[2]))
       p$labels$colour <- "Cluster"
       p
     # Otherwise a normal plot
