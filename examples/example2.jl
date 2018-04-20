b303c3e2a91856f9db3bd60f4f3928663ec73764
Kira Aveline Selby <kaselby@uwaterloo.ca>
2018-04-20 17:03:55 -0400

    test

---
+++ examples/example2.jl
@@ -0,0 +1,46 @@
+
+using DirichletMixtureModels
+
+importall DirichletMixtureModels
+
+import Distributions:
+        UnivariateDistribution,
+        MultivariateDistribution,
+        Exponential,
+        Normal,
+        NormalCanon,
+        MvNormal,
+        NormalStats,
+        rand,
+        suffstats,
+        pdf
+
+import ConjugatePriors:
+    NormalGamma,
+    NormalWishart,
+    posterior_canon,
+    rand
+
+
+
+function nc1_pdf(y::Float64,μ,λ,μ0,n0,α0,β0)
+  pdf(NormalCanon(λ*μ, λ), y)
+end
+function nc1_pdf(y::Array{Float64},μ,λ,μ0,n0,α0,β0)
+  pdf.(NormalCanon(λ*μ, λ), y)
+end
+function nc1_post(Y::Array{Float64,1}, μ0,n0,α0,β0)
+  p=posterior_canon(NormalGamma(μ0,n0,α0,β0),suffstats(Normal,Y))
+  rand(p)
+end
+function nc1_post(y::Float64, μ0,n0,α0,β0)
+  p=posterior_canon(NormalGamma(μ0,n0,α0,β0),suffstats(Normal,[y]))
+  rand(p)
+end
+function nc1_pri(μ0,n0,α0,β0)
+  rand(NormalGamma(μ0,n0,α0,β0))
+end
+function nc1_marg(y::Float64,μ0,n0,α0,β0)
+  gamma(α0+1./2)/gamma(α0) * sqrt(n0/(n0+1)) * 1/sqrt(2*π) * β0^α0 /
+    (β0+n0/2/(n0+1)*(y-μ0)^2)^(α0+1./2)
+end
